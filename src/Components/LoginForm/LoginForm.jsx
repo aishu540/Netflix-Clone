8a31411d15bf4e4bc7d2d9d7a59758e9b7542ed1
Aishwarya Kotur <aishwaryakotur797@gmail.com>
2026-07-14 17:25:58 +0530

    the react form is created

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,43 +1,51 @@
-import { Button, Form, Input } from "antd";
+
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const LoginForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-// the form is created
-  const addStudent = async () => {
-    try {
-     
-        const students = await axios.post(
-        "https://jsonplaceholder.typicode.com/users",
-        {
-          name: name,
-          email: email,
-        },
-      );
-     console.log(students.data)
-    } catch (error) {
-        console.log(error.message)
-    }
-  };
+  const [formData,setFormData]=useState({
+    firstName:"",
+    email:""
+  })
 
+  
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+  }
+  useEffect(()=>{
+    handleSubmit()
+  },[])
+  const handleSubmit=async (e)=>{
+    e.preventDefault()
+       try{
+        const response=await axios.post("https://dummyjson.com/users/add",formData)
+        setFormData({
+          firstName:"",
+          email:""
+        })
+        alert("the new data added successfully")
+       }
+       catch(error){
+        console.log(error.message)
+       }
+  }
   return (
     <div>
-      <Form onFinish={addStudent}>
-        <Form.Item label="Name" type="text">
-          <Input placeholder="Please enter your name" onChange={(e)=>setName(e.target.value)} />
-        </Form.Item>
+     <form onSubmit={handleSubmit}>
 
-        <Form.Item label="Email" type="email">
-          <Input placeholder="Please enter your email" onChange={(e)=>setEmail(e.target.value)} />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <div>
+        <p>First Name:</p>
+        <input type="text" placeholder="Please Enter your name" name="firstName" onChange={handleChange}/>
+      </div>
+      <div>
+        <p>Email:</p>
+        <input type="email" placeholder="Please Enter your Email" name="email" onChange={handleChange}/>
+      </div>
+      <button type="submit">Submit</button>
+     </form>
     </div>
   );
 };
